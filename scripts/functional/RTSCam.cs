e330e72042873d67d30c7634c2ceae5fdd81ee92
Kevin Lee Cardoza <49277166+Kiboot@users.noreply.github.com>
2024-03-17 23:33:47 +0800

    Testing unit spawning - unsuccessful for today

--- scripts/functional/RTSCam.cs
+++ scripts/functional/RTSCam.cs
@@ -42,7 +42,10 @@ public partial class RTSCam : Node3D
 	
 
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready(){}
+	public override void _Ready(){
+		cam = (Camera3D)GetNode("camera_socket/camera");
+		camSkt = (Node3D)GetNode("camera_socket");
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
